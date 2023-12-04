d7f78b743cff7b534e9fc6a3aaf8e63f4ad29e01
Evan Blair <evan@Evans-MacBook-Air.local>
2023-12-04 15:17:52 +0000

    day 4 part 1

--- 2023/4/solution.py
+++ 2023/4/solution.py
@@ -1,9 +1,4 @@
-def get_score(winners):
-    total = 1
-    for x in range(1, winners):
-        total *= 2
-    return total
-
+from functools import reduce
 
 final_total = 0
 
@@ -11,20 +6,11 @@ with open('data/test2.txt', 'r') as file:
     lines = file.read().split('\n')
     for y, row in enumerate(lines):
         parts = row.split(':')
-        card_no = parts[0][-1]
         numbers = parts[1].split('|')
         winning_nos = numbers[0].strip().split()
         numbers_you_have = numbers[1].strip().split()
         intersection = list(set(winning_nos) & set(numbers_you_have))
-        score = 0
         winners = len(intersection)
-        if winners > 0:
-            if winners == 1:
-                score += 1
-            elif winners == 2:
-                score += 2
-            else:
-                score += get_score(winners)
-        print(intersection, winners, score)
-        final_total += score
+        final_total += winners if winners <= 2 else reduce(lambda x, _: x * 2, range(winners - 1), 1)
+
 print('final_total', final_total)
